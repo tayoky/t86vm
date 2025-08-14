6632f82df775fefce890631eea3ab7c27f52b1d1
tayoky <tayoky4848@gmail.com>
2025-08-14 21:32:27 +0200

    main : refactor argument parsing

--- src/main.c
+++ src/main.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include "t86vm.h"
 
+#define OPT(str) !strcmp(argv[i],str)
+
 int main(int argc,char **argv){
-	int c;
 	t86vm_ctx_t ctx;
 	memset(&ctx,0,sizeof(ctx));
 	ctx.ram_size = 1024 * 1024;
-	while((c = getopt(argc,argv,"mf:")) != -1){
-		switch(c){
-		case '?':
-			return 1;
-		case 'm':
-			break;
-		case 'f':
-			ctx.floppy = fopen(optarg,"r+");
+	for(int i=1; i<argc; i++){
+		if(OPT("-f")  || OPT("--floppy")){
+			i++;
+			if(i>=argc)goto missing_operand;
+			ctx.floppy = fopen(argv[i],"r+");
 			if(!ctx.floppy){
-				perror(optarg);
+				perror(argv[i]);
 				return 1;
 			}
-			break;
+			continue;
 		}
+		if(OPT("-m") || OPT("--memory")){
+			i++;
+			ctx.ram_size = strtoul(argv[i],NULL,0);
+			continue;
+		}
+		error("unknow option : %s",argv[i]);
+		return 1;
+missing_operand:
+		error("missing operand");
+		return 1;
 	}
 
 	ctx.ram = malloc(ctx.ram_size);
