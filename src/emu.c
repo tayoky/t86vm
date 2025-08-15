7fe2a7d80d0e5e6888590eab791b4fd667a1f156
tayoky <tayoky4848@gmail.com>
2025-08-15 16:24:02 +0200

    emu : automatic bios address

--- src/emu.c
+++ src/emu.c
@@ -3,8 +3,8 @@
 #include "t86vm.h"
 
 int emul(t86vm_ctx_t *ctx){
-	//i belive the bios is loaded at 0xf0000
-	ctx->bios->base = 0xf0000;
+	//automatic bios address based on size
+	ctx->bios->base = 0x100000 - ctx->bios->size;
 	cpu8086_reset(&ctx->cpu);
 	printf("%d\n",cpu8086_emu(ctx));
 	return 0;
